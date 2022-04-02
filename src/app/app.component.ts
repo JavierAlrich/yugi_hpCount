82e50db26004fdb6305395b7493c0336e2cd18ba
Francisco Javier Omedes Alrich <javieralrich@gmail.com>
2022-04-02 01:24:03 -0600

    git adaptations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'yugi-hp';
+  points = 8000;
+  counterPts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  
+
+  pointsCalcAdd(pts){
+    return this.points += parseFloat(pts);
+  }
+
+  pointsCalcSub(pts){
+    if(this.points > 0){
+      return this.points -= pts;
+    }else{
+      return this.points = 0;
+    }
+  }
+
+  counterAdd(position){
+    return this.counterPts[position] += 1;
+  }
+
+  counterSub(position){
+    if(this.counterPts[position] > 0){
+      return this.counterPts[position] -= 1;
+    }else{
+      this.counterPts[position] = 0;
+    }
+  }
+
 }
